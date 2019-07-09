3e2be1f4fadfbed642c40d1cb4563e9a153ef648
DanielReis <idanielreiss@gmail.com>
2019-07-09 16:07:48 -0300

    commit inicial

---
+++ test.js
@@ -0,0 +1,19 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+const data = require('./config.json')
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+  clientId: data.spotify_id,
+  clientSecret: data.spotify_secret,
+  redirectUri: 'http://localhost/',
+  accessToken: 'BQA7b4G19Efh7VfjxOlsj6nD18WV7gwrYoEaB0P4ciCLJD6JP-ib_e99XEr9lLK1Y_oCUyOUB1zQ24o7u4vcd2vO2ctTIYwTm1eK9OzEgZt4x7e12MWrT9QgU5hxL_iTciOahVs5yIScQTgupHPVYirwb7aYSk70X09wTABr5HAyLpYFP0BDJxWgR1UtwYevWTQSNzSsSfzx_7R5NSPFPks5EChvPlHwaWZzXMkNmu3T6g'
+});
+
+
+spotifyApi.addTracksToPlaylist('21JDnkT9kCsqkeTWX6yz4d', 
+["spotify:track:4iV5W9uYEdYUVa79Axb7Rh", 
+"spotify:track:1301WleyT98MSxVHPZCA6M"])
+  .then(function(data) {
+    console.log('Added tracks to playlist!');
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
---
+++ utils.js
@@ -0,0 +1,140 @@
+const $ = require("jquery");
+const emotes = require("./emotes.json");
+const config = require("./config.json");
+const moment = require('moment');
+
+function getBadges(badges) {
+  let html = "";
+  if (!badges) {
+    return "";
+  }
+  if (badges.length !== 0) {
+    for (let key in badges) {
+      html += `<img src="./images/${key}.png" width="20"> `;
+    }
+    return html;
+  }
+}
+
+function formatName(name, color) {
+  return `<strong style="color: ${
+    color !== "" ? color : "#1E90FF"
+  }">${name}: </strong>`;
+}
+function showImage(img_url) {
+  return `<img src="${img_url}" width="250"></p>`;
+}
+
+function filterIcons(msg) {
+  emotes.forEach(function(item, index) {
+    while (msg.search(item.regex) >= 0) {
+      msg = msg.replace(item.regex, "<img src='" + item.image_url + "'>");
+    }
+  });
+  return msg;
+}
+
+function message(badges, name, color, message, regex = false) {
+  document
+    .getElementById("chat")
+    .insertAdjacentHTML(
+      "beforeend",
+      `<p>${getBadges(badges)} ${formatName(name, color)} ${message}</p>`
+    );
+}
+
+function popChat() {
+  if ($("#chat p").length >= 30) {
+    $("#chat p")[0].remove();
+  }
+}
+// function clearChat() {
+//   let chats = document.querySelectorAll("#chat p");
+//   if (chats.length != 0) {
+//     chats.forEach(chat => {
+//       chat.remove();
+//     });
+//   }
+// }
+
+function manageChat(chatter, msg) {
+  popChat();
+  getStreamInformation();
+  let cmd = msg.split(" ");
+  if (cmd) {
+    switch (cmd[0]) {
+      case "!image":
+        message(
+          chatter.badges,
+          chatter.username,
+          chatter.color,
+          showImage(cmd[1])
+        );
+        break;
+
+      default:
+        message(
+          chatter.badges,
+          chatter.username,
+          chatter.color,
+          filterIcons(msg),
+          false
+        );
+        break;
+    }
+
+    $("#chat").animate({ scrollTop: $("#chat").prop("scrollHeight") }, 1);
+  }
+}
+
+
+
+const getStreamInformation = () => {
+  let headers = new Headers();
+  
+  headers.append("Client-ID", config.client_id);
+  fetch(`https://api.twitch.tv/helix/streams?user_id=${config.user_id}`, {
+    headers: headers
+  })
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+      const user = res.data[0];
+      const uptime = moment().diff(moment(user.started_at));
+      var duration = moment.duration(uptime);
+      document.getElementById("title").innerHTML = user.title;
+      document.getElementById("streamer-name").innerHTML = user.user_name;
+      document.getElementById("viewers").innerHTML = user.viewer_count;
+      document.getElementById("uptime").innerHTML = duration.hours()  + "h " + duration.minutes() + " m"
+      eae = user.thumbnail_url
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+function getChatters(channel) {
+  fetch(`http://tmi.twitch.tv/group/user/${channel}/chatters`)
+    .then(res => res.json())
+    .then(res => {
+      const ul = document.createElement("ul");
+      Object.keys(res.chatters).map((val, i) => {
+        res.chatters[val].map(user => {
+          let li = document.createElement("li");
+          li.appendChild(document.createTextNode(user));
+          ul.appendChild(li);
+        });
+      });
+      document.querySelector("#showChatters").dataset.content = ul.outerHTML;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+module.exports = {
+  manageChat,
+  getStreamInformation,
+  getChatters,
+  message
+};
